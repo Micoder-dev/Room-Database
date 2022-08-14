df19a3a80c37f035e222846e788424ad44012d7d
Micoder-dev <micoder.com@gmail.com>
2022-08-14 21:13:52 +0530

    Entity Class "creation of table"

---
+++ app/src/main/java/com/example/roomdatabase/db/DatabaseHelper.java
@@ -0,0 +1,4 @@
+package com.example.roomdatabase.db;
+
+public class DatabaseHelper {
+}
---
+++ app/src/main/java/com/example/roomdatabase/db/entity/Contact.java
@@ -0,0 +1,62 @@
+package com.example.roomdatabase.db.entity;
+
+public class Contact {
+
+    // 1 - Constants for Database
+    public static final String TABLE_NAME = "contacts";
+    public static final String COLUMN_ID = "contact_id";
+    public static final String COLUMN_NAME = "contact_name";
+    public static final String COLUMN_EMAIL = "contact_email";
+
+    // 2 - Variables
+    private String name;
+    private String email;
+    private int id;
+
+    // 3 - Constructors
+    private Contact() {
+
+    }
+
+    private Contact(String name, String email, int id) {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+    }
+
+    // 4 - Getters & Setters
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // SQL Query: Creating the Table
+    public static final String CREATE_TABLE =
+            "CREATE TABLE " + TABLE_NAME + "("
+            + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT,"
+            + COLUMN_NAME + " TEXT,"
+            + COLUMN_EMAIL + " DATETIME DEFAULT CURRENT TIMESTAMP"
+            + ")";
+
+}
