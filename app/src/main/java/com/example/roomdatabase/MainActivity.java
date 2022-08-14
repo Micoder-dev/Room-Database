59a35b4ad0b55b099bfa8511953811e759def3b7
Micoder-dev <micoder.com@gmail.com>
2022-08-15 00:05:43 +0530

    Initializing the Widgets "are initialized in MainActivity & delete,update,create contact methods are still not initialized"

--- app/src/main/java/com/example/roomdatabase/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase/MainActivity.java
@@ -1,21 +1,138 @@
 package com.example.roomdatabase;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.roomdatabase.adapter.ContactsAdapter;
+import com.example.roomdatabase.db.DatabaseHelper;
+import com.example.roomdatabase.db.entity.Contact;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Variables
+    // NO ROOM Database project
+    // Using SQLite
 
+    // Variables
+    private ContactsAdapter contactsAdapter;
+    private ArrayList<Contact> contactArrayList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private DatabaseHelper db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("My Contacts Manager");
+
+        // RecyclerView
+        recyclerView = findViewById(R.id.recycler_view_contacts);
+
+        db = new DatabaseHelper(this);
+
+        // Contacts List
+        contactArrayList.addAll(db.getAllContacts());
+
+        contactsAdapter = new ContactsAdapter(this, contactArrayList, MainActivity.this);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(contactsAdapter);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addAndEditContacts(false, null, -1);
+            }
+        });
+
+
     }
 
-    // NO ROOM Database project
-    // Using SQLite
+    public void addAndEditContacts(final boolean isUpdated, final Contact contact, final int position) {
+
+        LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
+        View view = layoutInflater.inflate(R.layout.layout_add_contact, null);
+
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(MainActivity.this);
+        alertDialogBuilder.setView(view);
+
+        TextView contactTitle = view.findViewById(R.id.new_contact_title);
+        final EditText newContact = view.findViewById(R.id.name);
+        final EditText contactEmail = view.findViewById(R.id.email);
+
+        contactTitle.setText(!isUpdated ? "Add New Contact" : "Edit Contact");
+
+        if (isUpdated && contact != null) {
+            newContact.setText(contact.getName());
+            contactEmail.setText(contact.getEmail());
+        }
+
+        alertDialogBuilder.setCancelable(false)
+                .setPositiveButton(isUpdated ? "Update" : "Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setNegativeButton("Delete",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                if (isUpdated) {
+                                    DeleteContact(contact, position);
+                                }else {
+                                    dialogInterface.cancel();
+                                }
+                            }
+                        }
+                );
+
+        final AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+
+        alertDialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (TextUtils.isEmpty(newContact.getText().toString())) {
+                    Toast.makeText(MainActivity.this, "Please enter a name", Toast.LENGTH_SHORT).show();
+                    return;
+                }else {
+                    alertDialog.dismiss();
+                }
+                if (isUpdated && contact != null) {
+                    UpdateContact(newContact.getText().toString(), contactEmail.getText().toString(), position);
+                }else {
+                    CreateContact(newContact.getText().toString(), contactEmail.getText().toString());
+                }
+            }
+        });
+
+
+
+    }
+
 
 }
