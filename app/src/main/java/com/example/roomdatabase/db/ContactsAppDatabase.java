a2ee6e21b306e3f70c33592036c936274a680e70
Micoder-dev <micoder.com@gmail.com>
2022-08-15 16:09:53 +0530

    Database Class (@Database) "linking the DAO with our database"

---
+++ app/src/main/java/com/example/roomdatabase/db/ContactsAppDatabase.java
@@ -0,0 +1,14 @@
+package com.example.roomdatabase.db;
+
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+import com.example.roomdatabase.db.entity.Contact;
+
+@Database(entities = {Contact.class}, version = 1)
+public abstract class ContactsAppDatabase extends RoomDatabase {
+
+    // Linking the DAO with our Database
+    public abstract ContactDAO getContactDAO();
+
+}
