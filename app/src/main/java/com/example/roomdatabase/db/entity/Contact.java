4d7d66cb7a7a11cedfada7c514c70066aaac7a0c
Micoder-dev <micoder.com@gmail.com>
2022-08-15 12:55:22 +0530

    Creating Entity Class

--- app/src/main/java/com/example/roomdatabase/db/entity/Contact.java
+++ app/src/main/java/com/example/roomdatabase/db/entity/Contact.java
@@ -1,19 +1,33 @@
 package com.example.roomdatabase.db.entity;
 
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "contacts")
 public class Contact {
 
     // 1 - Constants for Database
-    public static final String TABLE_NAME = "contacts";
-    public static final String COLUMN_ID = "contact_id";
-    public static final String COLUMN_NAME = "contact_name";
-    public static final String COLUMN_EMAIL = "contact_email";
+    // public static final String TABLE_NAME = "contacts";
+    // public static final String COLUMN_ID = "contact_id";
+    // public static final String COLUMN_NAME = "contact_name";
+    // public static final String COLUMN_EMAIL = "contact_email";
 
     // 2 - Variables
+    @ColumnInfo(name = "contact_name")
     private String name;
+
+    @ColumnInfo(name = "contact_email")
     private String email;
+
+    @ColumnInfo(name = "contact_id")
+    @PrimaryKey(autoGenerate = true)
     private int id;
 
     // 3 - Constructors
+    @Ignore
     public Contact() {
 
     }
@@ -51,12 +65,12 @@ public class Contact {
         this.id = id;
     }
 
-    // SQL Query: Creating the Table
-    public static final String CREATE_TABLE =
-            "CREATE TABLE " + TABLE_NAME + "("
-            + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
-            + COLUMN_NAME + " TEXT,"
-            + COLUMN_EMAIL + " DATETIME DEFAULT CURRENT_TIMESTAMP"
-            + ")";
+//    // SQL Query: Creating the Table
+//    public static final String CREATE_TABLE =
+//            "CREATE TABLE " + TABLE_NAME + "("
+//            + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+//            + COLUMN_NAME + " TEXT,"
+//            + COLUMN_EMAIL + " DATETIME DEFAULT CURRENT_TIMESTAMP"
+//            + ")";
 
 }
